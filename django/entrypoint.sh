f819f0b258943169b45058a1fe4d999bc9e6a20d
Andres Miguel Jimenez Rios <andres.miguel.jimenez.rios@everis.com>
2019-04-06 13:59:34 +0200

    All services works independiently

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
+#https://stackoverflow.com/questions/43925487/how-to-run-gunicorn-on-docker
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --email andjimrio@alum.us.es --username andjimrio
-python manage.py collectstatic
-gunicorn project.wsgi -b 0.0.0.0:8000
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('andjimrio', 'andjimrio@alum.us.es', 'andjimrio')"
+#python manage.py createsuperuser --email andjimrio@alum.us.es --username andjimrio --noinput
+python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:8000
+#gunicorn ltnews.wsgi -b 0.0.0.0:8000
